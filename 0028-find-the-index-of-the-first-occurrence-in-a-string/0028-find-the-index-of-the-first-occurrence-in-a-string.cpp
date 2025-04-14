d012100617622fba85659653300da77ba422cacd
Abraham Alefew - Cosmocoder <75578348+Abe-Alefew@users.noreply.github.com>
2025-04-14 13:10:30 -0700

    Time: 0 ms (100%), Space: 8.9 MB (13.18%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int n= haystack.length();
+        int m = needle.length();
+        int i = 0;
+        while(i <= n-m){
+            if(haystack.substr(i, m) == needle){
+                return i;
+            }
+            i++;
+        }
+        return -1;
+    }
+};

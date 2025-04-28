9cc2f8916c9ddbe8ea7d43ceb0cd4544a25829f8
Abraham Alefew - Cosmocoder <75578348+Abe-Alefew@users.noreply.github.com>
2025-04-28 13:38:27 -0700

    Time: 10 ms (18.85%), Space: 74.4 MB (31.21%) - LeetHub

--- 0878-shifting-letters/0878-shifting-letters.cpp
+++ 0878-shifting-letters/0878-shifting-letters.cpp
@@ -8,13 +8,13 @@ public:
         int n = s.size();
         vector<int> totalShifts(n, 0);
 
-        // Calculate total shifts needed for each character (starting from the back)
+        
         totalShifts[n-1] = shifts[n-1] % 26;
         for (int i = n-2; i >= 0; i--) {
             totalShifts[i] = (shifts[i] + totalShifts[i+1]) % 26;
         }
 
-        // Shift each character
+        
         for (int i = 0; i < n; i++) {
             s[i] = (s[i] - 'a' + totalShifts[i]) % 26 + 'a';
         }

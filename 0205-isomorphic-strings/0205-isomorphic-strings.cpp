182cc64b57d5687f82879dabbce24457ccc58740
Abraham Alefew - Cosmocoder <75578348+Abe-Alefew@users.noreply.github.com>
2025-05-23 23:58:12 +0300

    Time: 1 ms (57.53%), Space: 9.3 MB (56.38%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char,char> map_s;
+        unordered_map<char,char> map_t;
+
+        for(int i= 0; i < s.length(); i++){
+            char cs = s[i];
+            char ct = t[i];
+            if(map_s.count(cs) && map_s[cs]!= ct) return false;
+            if(map_t.count(ct) && map_t[ct]!= cs) return false;
+
+            map_s[cs] = ct;
+            map_t[ct] = cs;
+        }
+    
+        return true;
+
+    }
+};

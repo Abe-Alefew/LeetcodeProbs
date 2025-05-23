2c4ba1078eb0f2fc8c8025c1c631fe7a32a96f54
Abraham Alefew - Cosmocoder <75578348+Abe-Alefew@users.noreply.github.com>
2025-05-24 00:08:31 +0300

    Time: 0 ms (100%), Space: 9.3 MB (56.38%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -7,8 +7,8 @@ public:
         for(int i= 0; i < s.length(); i++){
             char cs = s[i];
             char ct = t[i];
-            if(map_s.count(cs) && map_s[cs]!= ct) return false;
-            if(map_t.count(ct) && map_t[ct]!= cs) return false;
+            if(map_s.find(cs)!= map_s.end() && map_s[cs]!= ct) return false;
+            if(map_t.find(ct) != map_t.end() && map_t[ct]!= cs) return false;
 
             map_s[cs] = ct;
             map_t[ct] = cs;

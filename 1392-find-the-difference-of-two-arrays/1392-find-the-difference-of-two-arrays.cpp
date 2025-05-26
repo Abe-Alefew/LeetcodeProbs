e1b6d8a5ed997536d5f48be880a0992046f0f2a8
Abraham Alefew - Cosmocoder <75578348+Abe-Alefew@users.noreply.github.com>
2025-05-26 15:30:22 +0300

    Time: 37 ms (16.23%), Space: 36.8 MB (81.05%) - LeetHub

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.cpp
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.cpp
@@ -4,29 +4,24 @@ public:
         unordered_map<int,int> map1;
         unordered_map<int,int> map2;
 
-        vector<vector<int>> output;
-        for(int num:nums1){
-            map1[num]++;
-        }
-        for(int num:nums2){
-            map2[num]++;
-        }
+        vector<vector<int>> output(2);
+        for(int num:nums1) map1[num]++;
+        for(int num:nums2) map2[num]++;
         vector<int> answer;
         for(int num:nums1){
-            if(find(answer.begin(),answer.end(), num)!= answer.end()) continue;
+            if(find(output[0].begin(),output[0].end(), num)!= output[0].end()) continue;
             if(!map1.count(num) || !map2.count(num)){
-                answer.push_back(num);
+                output[0].push_back(num);
             }
         }
-        output.push_back(answer);
-        vector<int> answer1;
+        
         for(int num:nums2){
-             if(find(answer1.begin(),answer1.end(), num)!= answer1.end()) continue;
+             if(find(output[1].begin(),output[1].end(), num)!= output[1].end()) continue;
             if(!map1.count(num)|| !map2.count(num)){
-                answer1.push_back(num);
+                output[1].push_back(num);
             }
         }
-        output.push_back(answer1);
+      
         return output;
     }
 };

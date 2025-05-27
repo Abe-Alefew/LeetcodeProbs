003f354631e63a708d83023d58c29f52900c8415
Abraham Alefew - Cosmocoder <75578348+Abe-Alefew@users.noreply.github.com>
2025-05-27 23:31:00 +0300

    Time: 0 ms (100%), Space: 18 MB (22.74%) - LeetHub

--- 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.cpp
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
-    
     bool isAnagram(string s, string t) {
-        unordered_map<char,int> map;
-        unordered_map<char,int> map1;
-        if(s.length() != t.length()) return false;
-        for(char c:s){
-            map[c]++;
+        if (s.length() != t.length()) return false;
+        vector<int> count(26, 0);
+        for (int i = 0; i < s.length(); ++i) {
+            count[s[i] - 'a']++;
+            count[t[i] - 'a']--;
         }
-        for(char c:t){
-            map1[c]++;
-        }
-        for(int i = 0; i< s.length(); i++){
-            if(map[s[i]] != map1[s[i]]) return false;
+        for (int i = 0; i < 26; ++i) {
+            if (count[i] != 0) return false;
         }
         return true;
     }
 
     vector<string> removeAnagrams(vector<string>& words) {
         vector<string> result;
-        result.push_back(words[0]); 
+        result.push_back(words[0]); // Always keep the first word
 
         for (int i = 1; i < words.size(); ++i) {
             if (!isAnagram(words[i], result.back())) {

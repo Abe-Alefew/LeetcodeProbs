605e271ea8d7e509a6fc461aaa938536d41cb52b
Abraham Alefew - Cosmocoder <75578348+Abe-Alefew@users.noreply.github.com>
2025-06-06 23:24:38 +0300

    Time: 0 ms (100%), Space: 26.4 MB (16.48%) - LeetHub

--- 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.cpp
+++ 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.cpp
@@ -2,14 +2,13 @@ class Solution {
 public:
     vector<int> getSneakyNumbers(vector<int>& nums) {
         unordered_map<int,int> map;
+        vector<int> output;
         for(int n:nums){
             map[n]++;
+            if(map[n] > 1) output.push_back(n);
         }
-        vector<int> output;
-        for(pair<int,int> p:map){
-            if(p.second == 2) output.push_back(p.first);
-        }
-
+        
+        
         return output;
 
     }

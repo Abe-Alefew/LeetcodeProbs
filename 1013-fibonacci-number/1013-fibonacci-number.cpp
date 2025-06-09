f409dfabf38d06e1fecea0b04e8122f755219369
Abraham Alefew - Cosmocoder <75578348+Abe-Alefew@users.noreply.github.com>
2025-06-09 11:48:13 +0300

    Time: 3 ms (64.82%), Space: 8.3 MB (11.54%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,9 +1,34 @@
 class Solution {
 public:
-    int fib(int n) {
-        
-        if (n == 1 || n == 0) return n;
+    vector<vector<int>> mult(vector<vector<int>> mat1, vector<vector<int>> mat2) {
+        return {
+            {
+                mat1[0][0]*mat2[0][0] + mat1[0][1]*mat2[1][0],
+                mat1[0][0]*mat2[0][1] + mat1[0][1]*mat2[1][1]
+            },
+            {
+                mat1[1][0]*mat2[0][0] + mat1[1][1]*mat2[1][0],
+                mat1[1][0]*mat2[0][1] + mat1[1][1]*mat2[1][1]
+            }
+        };
+    }
 
-        return (fib(n-1) + fib(n-2));
+    vector<vector<int>> mat_pow(vector<vector<int>> mat, int power) {
+        vector<vector<int>> res = {{1, 0}, {0, 1}}; // Identity matrix
+        while (power) {
+            if (power % 2 == 1) {
+                res = mult(res, mat);
+            }
+            mat = mult(mat, mat);
+            power /= 2;
+        }
+        return res;
+    }
+
+    int fib(int n) {
+        if (n == 0) return 0;
+        vector<vector<int>> base = {{1, 1}, {1, 0}};
+        vector<vector<int>> res = mat_pow(base, n - 1);
+        return res[0][0];
     }
 };

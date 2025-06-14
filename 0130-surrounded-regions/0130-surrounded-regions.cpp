f218683bad37beba148dec0e3af920c7fb5f9b3f
Abraham Alefew - Cosmocoder <75578348+Abe-Alefew@users.noreply.github.com>
2025-06-14 16:19:14 +0300

    Time: 0 ms (100%), Space: 13.9 MB (83.82%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -6,7 +6,7 @@ public:
 
         if (r < 0 || c < 0 || r >= rows || c >= cols || board[r][c] != 'O') return;
 
-        board[r][c] = 'T'; 
+        board[r][c] = '#'; // temporary marking
         capture(r + 1, c, board);
         capture(r - 1, c, board);
         capture(r, c + 1, board);
@@ -14,7 +14,7 @@ public:
     }
 
     void solve(vector<vector<char>>& board) {
-        if (board.empty() || board[0].empty()) return;
+        if (board.empty()) return;
 
         int r = board.size();
         int c = board[0].size();
@@ -24,17 +24,17 @@ public:
             if (board[i][0] == 'O') capture(i, 0, board);
             if (board[i][c - 1] == 'O') capture(i, c - 1, board);
         }
-        //Capturing the surrounded regions
+        // Capturing the surrounded regions
         for (int j = 0; j < c; j++) {
             if (board[0][j] == 'O') capture(0, j, board);
             if (board[r - 1][j] == 'O') capture(r - 1, j, board);
         }
 
-        //Uncapturing unsurrounded regions
+        // Uncapturing unsurrounded regions
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
                 if (board[i][j] == 'O') board[i][j] = 'X';
-                else if (board[i][j] == 'T') board[i][j] = 'O';
+                else if (board[i][j] == '#') board[i][j] = 'O';
             }
         }
     }
